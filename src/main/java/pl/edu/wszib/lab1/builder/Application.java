c00ba19ca4207d92373ba5d6cca6c9b11047c8f0
ppustelnik <pawel.pustelnik@exorigo-upos.pl>
2022-10-16 11:44:29 +0200

    exceptions

--- src/main/java/pl/edu/wszib/lab1/builder/Application.java
+++ src/main/java/pl/edu/wszib/lab1/builder/Application.java
@@ -1,5 +1,12 @@
 package pl.edu.wszib.lab1.builder;
 
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.net.ConnectException;
+import java.net.SocketTimeoutException;
+import java.util.Random;
+import java.util.logging.Logger;
+
 public class Application {
 
     public static void main(String[] args) {
@@ -16,6 +23,20 @@ public class Application {
         personBuilder.address(address);
         MyRecord myRecord = new MyRecord("test", "myNewVar");
         myRecord.myNewVar();
+
+        try {
+            myMethod();
+        } catch (ConnectException | SocketTimeoutException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    private static void myMethod() throws ConnectException, SocketTimeoutException {
+        if (new Random().nextBoolean()) {
+            throw new ConnectException();
+        } else {
+            throw new SocketTimeoutException();
+        }
     }
 }
 

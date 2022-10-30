a595933d84d1857c3e3e399ca8dbab8d19550503
ppustelnik <pawel.pustelnik@exorigo-upos.pl>
2022-10-30 09:58:11 +0100

    singleton multithread - basic impl

--- src/main/java/pl/edu/wszib/lab1/singleton/Application.java
+++ src/main/java/pl/edu/wszib/lab1/singleton/Application.java
@@ -1,11 +1,28 @@
 package pl.edu.wszib.lab1.singleton;
 
-import java.time.LocalDateTime;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Application {
+    static ConnectionLazy instance1;
+    static ConnectionLazy instance;
 
     public static void main(String[] args) {
-//        EnumIsSingleton.INSTANCE_1
-
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        executorService.submit(() -> {
+            instance1 = ConnectionLazy.getInstance();
+        });
+        instance = ConnectionLazy.getInstance();
+        if (instance == instance1) {
+            System.out.println(instance);
+            System.out.println(instance1);
+            System.out.println("Instances are the same");
+        } else {
+            System.out.println(instance);
+            System.out.println(instance1);
+            System.out.println("Instances are different");
+        }
+        System.out.println("Instance count: " + ConnectionLazy.counter.get());
+        executorService.shutdown();
     }
 }
--- src/main/java/pl/edu/wszib/lab1/singleton/ConnectionLazy.java
+++ src/main/java/pl/edu/wszib/lab1/singleton/ConnectionLazy.java
@@ -1,15 +1,26 @@
 package pl.edu.wszib.lab1.singleton;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public final class ConnectionLazy {
     private static ConnectionLazy INSTANCE;
+    public static AtomicInteger counter = new AtomicInteger(0);
 
     private ConnectionLazy() {
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public static ConnectionLazy getInstance() {
+    public synchronized static ConnectionLazy getInstance() {
         if (INSTANCE == null) {
+            counter.incrementAndGet();
             INSTANCE = new ConnectionLazy();
         }
         return INSTANCE;
     }
+
+//    synchronized
 }
